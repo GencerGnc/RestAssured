c1b3e8a4dca69a1a9e35341421bf5da666c37d02
GencerGnc <gencer-genc@outlook.com>
2024-05-01 21:47:33 +0300

    ApiTestSpec created

--- src/test/java/_01_ApiTest.java
+++ src/test/java/_01_ApiTest.java
@@ -3,39 +3,40 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
+import static io.restassured.RestAssured.port;
+import static org.hamcrest.Matchers.*;
 
 public class _01_ApiTest {
 
     @Test
-    public void Test(){
+    public void Test() {
 
         given()
                 // hazırlık kodları buraya yazılır
-        .when()
+                .when()
                 //endpoint(url) metoduyla birlikte istek gönderilme aşaması
                 .then();
-                    //assertion test data işlemleri
+        //assertion test data işlemleri
 
     }
 
     @Test
-    public void statusCodeTest(){
+    public void statusCodeTest() {
 
         given()
-                    // gönderilecek bilgiler burada
+                // gönderilecek bilgiler burada
                 .when()
                 .get("https://api.zippopotam.us/us/90210")
                 .then()
                 .log().body() // gelen body kısmını ver all dersek herşeyi verir
                 .statusCode(200); // test kısmı assertion status 200
-                ;
+        ;
 
 
     }
+
     @Test
-    public void contentTypeTest(){
+    public void contentTypeTest() {
 
         given()
 
@@ -53,7 +54,7 @@ public class _01_ApiTest {
     }
 
     @Test
-    public void chechkCountryInResponsBody(){
+    public void chechkCountryInResponsBody() {
         given()
 
                 .when()
@@ -62,15 +63,16 @@ public class _01_ApiTest {
                 .log().body()
                 .statusCode(200)
                 .body("country", equalTo("United States")) // countryi dışarı almadan bulunduğu yeri yani path i
-                                                            // vererek içerde assertion yapmak HemCrest kütüphanesi sayesinde
+        // vererek içerde assertion yapmak HemCrest kütüphanesi sayesinde
 
         ;
     }
+
     // Soru : "http://api.zippopotam.us/us/90210"  endpoint indne dönen
     // place dizisinin ilk elemanının state değerinin  "California"
     // olduğunu doğrulayınız
     @Test
-    public void chechkCountryInResponsBody2(){
+    public void chechkCountryInResponsBody2() {
 
         given()
 
@@ -80,10 +82,9 @@ public class _01_ApiTest {
                 .log().body()
                 .statusCode(200)
                 .body("places[0].state", equalTo("California")) // countryi dışarı almadan bulunduğu yeri yani path i
-        // vererek içerde assertion yapmak HemCrest kütüphanesi sayesinde
+        // vererek içerde assertion yapmak HemCrest kütüphanesi sayesinde birinci elemani California mı
 
         ;
-
 
 
     }
@@ -93,7 +94,7 @@ public class _01_ApiTest {
     // olduğunu doğrulayınız
 
     @Test
-    public void test(){
+    public void test() {
 
 
         given()
@@ -107,11 +108,110 @@ public class _01_ApiTest {
         ;
 
 
+    }
+
+    // Soru : "http://api.zippopotam.us/us/90210"  endpoint in dönen
+    // place dizisinin dizi uzunluğunun 1 olduğunu doğrulayınız.
+    @Test
+    public void bodyArrayHasSizeTest() {
+        given()
+                // gönderilecek bilgiler burada
+                .when()
+                .get("https://api.zippopotam.us/us/90210")
+                .then()
+                //  .log().body()
+                .body("places", hasSize(1)) // dizi uzunluğu bir mi
+
+        ;
+
+    }
+
+    @Test
+    public void combiningTest() {
+
+        given()
+
+                .when()
+                .get("https://api.zippopotam.us/us/90210")
+                .then()
+                //  .log().body()
+                .body("places", hasSize(1)) // dizi uzunluğu bir mi
+                .body("places[0].state", equalTo("California")) // 1.index de California varmi
+                .body("places[0].'place name'", equalTo("Beverly Hills")) // 1.place in place name i Beverly Hills mi
+        ;
+
 
     }
 
 
+    @Test
+    public void pathParamTest(){
+
+        given()
+                //gönderilecek hazırlıklar
+
+                .pathParam("ulke","us")
+                .pathParam("postaKodu",90210)
+                .log().uri() // request linki ni göndermeden önce girebilirsin
+
+                .when()
+                .get("https://api.zippopotam.us/{ulke}/{postaKodu}")
+
+                .then()
+                .log().body()
+
+        ;
+
+    }
+
+    @Test
+    public void queryParamTest(){
+        given()
+                .param("page",1) //?page=1
+                .log().uri()
 
 
+
+                .when()
+                .get("https://gorest.co.in/public/v1/users")
+               // .get("https://gorest.co.in/public/v1/users?page=3")
+                .then()
+
+                .log().body()
+
+
+
+        ;
+    }
+
+    @Test
+    public void queryParamTest2(){
+        // https://gorest.co.in/public/v1/users?page=3
+        // bu linkteki 1 den 10 kadar sayfaları çağırdığınızda response daki donen page degerlerinin
+        // çağrılan page nosu ile aynı olup olmadığını kontrol ediniz.
+
+
+        for (int i = 1; i <=10 ; i++) {
+
+            given()
+
+                    .param("page",i)
+                    .log().uri()
+
+                    .when()
+                    .get("https://gorest.co.in/public/v1/users")
+
+
+                    .then()
+                    .body("meta.pagination.page", equalTo(i))
+
+
+
+            ;
+        }
+
+
+
+    }
 
 }
