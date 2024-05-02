824b231c0feec5f10f97ce44a61f13ea36a68042
GencerGnc <gencer-genc@outlook.com>
2024-05-02 21:43:41 +0300

    ApiTestPOJO with Jackson annotation created

---
+++ src/test/java/_04_ApiTestPOJO.java
@@ -0,0 +1,32 @@
+import Model.Location;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+
+
+public class _04_ApiTestPOJO {
+
+    @Test
+    public void extractJsonAll_POJO(){
+
+        Location locationNesnesi=
+                given()
+
+                        .when()
+                        .get("http://api.zippopotam.us/us/90210")
+
+                        .then()
+                        .extract().body().as(Location.class);
+        ;
+
+        System.out.println("locationNesnesi = " + locationNesnesi);
+        System.out.println("locationNesnesi.getPlaces() = " + locationNesnesi.getPlaces());
+        System.out.println("locationNesnesi.getPostcode() = " + locationNesnesi.getPostcode());
+
+
+    }
+}
